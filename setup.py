fef4683871e341be381bf5b880d2aa86857070b1
Ryan Clark <3845066+nikkel@users.noreply.github.com>
2024-07-16 00:34:26 +0100

    Updated version 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tornapi',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python library for interacting with the Torn API',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
